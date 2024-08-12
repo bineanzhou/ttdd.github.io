c9e9f2cb3489b377e04a469190420909cd76b83c
bineanzhou <bineanzhou@163.com>
2024-08-12 14:25:59 +0800

    修改上传脚本

--- data/crx/push_crx.sh
+++ data/crx/push_crx.sh
@@ -1,5 +1,7 @@
+#!/bin/bash
+pushd "$(dirname "$0")" > /dev/null
+echo "当前目录是: $(pwd)"
 adb shell mkdir -p /sdcard/android/data/com.qihoo.contents/files/crx/local;
 adb shell rm -rf /sdcard/android/data/com.qihoo.contents/files/crx/local/*;
 adb push *.crx /sdcard/android/data/com.qihoo.contents/files/crx/local;
-adb push Easy_QR_Code_1.0.7_v3.crx /sdcard/android/data/com.qihoo.contents/files/crx/local;
-adb push Global_Speed_3.0.9971.crx /sdcard/android/data/com.qihoo.contents/files/crx/local
+popd > /dev/null
